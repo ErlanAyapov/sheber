1b93b5d8736ced9c9c5af7d537655c55309277d2
ErlanAyapov <simsiti0009ggg@gmail.com>
2021-11-02 00:29:31 +0600

    co

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Article
-
+from maker.models import Product
 # def MainView(request):
 # 	return render(request, 'main/index.html')
 
@@ -9,7 +9,7 @@ class MainView(ListView):
 	model = Article
 	ordering = '-id'
 	template_name = 'main/index.html'
-	# def get_context_data(self, **kwargs):
-	# 	context = super(MainView, self).get_context_data(**kwargs)
-	# 	context['comment'] = Comment.objects.all()
-	# 	return context
+	def get_context_data(self, **kwargs):
+		context = super(MainView, self).get_context_data(**kwargs)
+		context['product'] = Product.objects.all()
+		return context
